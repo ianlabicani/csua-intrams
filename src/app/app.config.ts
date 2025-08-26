3e19be3beb1e7aa11ee94b9c8bfb9bc2f7ce3b0f
ianlabicani <labicaniian@gmail.com>
2025-08-26 08:28:29 +0800

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "csua-intrams", appId: "1:750994193369:web:4e14951627373b6c7377c8", storageBucket: "csua-intrams.firebasestorage.app", apiKey: "AIzaSyDMkCece45r8TPpTFmWSkU6z-1h2d9Pzio", authDomain: "csua-intrams.firebaseapp.com", messagingSenderId: "750994193369" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
